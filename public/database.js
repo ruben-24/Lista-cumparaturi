c1a79598466eecf05093d171f1689b8319ed63d5
ruben-24 <rubenalex15@yahoo.com>
2025-09-27 10:36:59 +0200

    Update database.js

--- public/database.js
+++ public/database.js
@@ -1,28 +1,27 @@
-// database.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-database.js";
 
+// 🔹 Înlocuiește cu datele tale din Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyAhN-DQQqWLo7s2SHMEHbp67P7mPqips3k",
-  authDomain: "lista--cumparaturi.firebaseapp.com",
-  databaseURL: "https://lista--cumparaturi-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "lista--cumparaturi",
-  storageBucket: "lista--cumparaturi.appspot.com",
+  apiKey: "API_KEY_TAU",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
   messagingSenderId: "1234567890",
   appId: "1:1234567890:web:abcdef123456"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const db = getDatabase(app);
 
-export function saveList(magazin, lista) {
-  set(ref(db, 'lists/' + magazin), lista);
+export function saveList(magazin, list) {
+  set(ref(db, 'lists/' + magazin), list);
 }
 
 export function subscribeList(magazin, callback) {
   const listRef = ref(db, 'lists/' + magazin);
   onValue(listRef, (snapshot) => {
-    const data = snapshot.val() || [];
-    callback(data);
+    callback(snapshot.val() || []);
   });
 }
