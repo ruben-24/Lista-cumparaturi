d2477800dd0bc7d0f0fa1b26872a60f529d833dd
ruben-24 <rubenalex15@yahoo.com>
2025-09-27 10:38:23 +0200

    Update database.js

--- public/database.js
+++ public/database.js
@@ -1,20 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-analytics.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-database.js";
 
-// 🔹 Înlocuiește cu datele tale din Firebase Console
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "API_KEY_TAU",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyAhN-DQQqWLo7s2SHMEHbp67P7mPqips3k",
+  authDomain: "lista--cumparaturi.firebaseapp.com",
+  databaseURL: "https://lista--cumparaturi-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lista--cumparaturi",
+  storageBucket: "lista--cumparaturi.firebasestorage.app",
+  messagingSenderId: "1017722987139",
+  appId: "1:1017722987139:web:9a00866e9b5ace247131b6",
+  measurementId: "G-045KJZYQ9T"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
+// Funcții pentru salvare și citire listă
 export function saveList(magazin, list) {
   set(ref(db, 'lists/' + magazin), list);
 }
